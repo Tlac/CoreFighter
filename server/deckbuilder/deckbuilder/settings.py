459af9af677faca8093440cb11542177bfcb193e
Tlac <thomaslac912@gmail.com>
2025-08-02 23:52:10 -0400

    Cleaned up settings

--- server/deckbuilder/deckbuilder/settings.py
+++ server/deckbuilder/deckbuilder/settings.py
@@ -16,9 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-(u1e1()8@3qz-i@*ijdq662fv_ih*ppyqec+n$y(7n8@rsxwqu"
 
